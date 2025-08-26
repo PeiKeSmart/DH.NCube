a844f0cc87266b756edea8bc88efaa023e9b83d7
猿人易 <qcjxberin@outlook.com>
2025-08-26 15:13:49 +0800

    备份与恢复时，传入取消令牌

--- DH.NCubeNC/Common/EntityController.cs
+++ DH.NCubeNC/Common/EntityController.cs
@@ -306,7 +306,7 @@ public partial class EntityController<TEntity, TModel>
 
         return act switch
         {
-            "Sync" => Backup(),
+            "Backup" => await Backup(),
             _ => await base.Develop(act),
         };
     }
